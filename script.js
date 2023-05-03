65459c8c18f3675c2966bfccbd07447fdbf029bc
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-05-03 10:58:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ let quizStarted = false;
 let timer;
 let totalTime = 60; // seconds
 let answeredQuestions = [];
+let countdownPerQuestion = false; // set to true if the countdown should happen for each question
 
 const totalQuestions = questions.length;
 
@@ -38,9 +39,9 @@ function startQuiz() {
   document.getElementById("start-btn").classList.add("d-none");
   document.getElementById("quiz-container").classList.remove("d-none");
   showQuestion();
-  startTimer();
-  // Get the total number of questions
-  totalQuestions = document.querySelectorAll('.quiz-question').length;
+  if (!countdownPerQuestion) { // start timer only if countdown is for the whole test
+    startTimer();
+  }
 }
 
 // Function to show the current question
@@ -106,54 +107,80 @@ function disableAnswerButtons() {
 // Function to enable answer buttons for next question
 function enableAnswerButtons() {
   const answerButtons = document.getElementsByClassName("answer-option");
-  for (let i= 0; i < answerButtons.length; i++) {
-answerButtons[i].addEventListener("click", checkAnswer);
-answerButtons[i].classList.remove("disabled", "correct", "incorrect");
-}
+  for (let i = 0; i < answerButtons.length; i++) {
+    answerButtons[i].addEventListener("click", checkAnswer);
+    answerButtons[i].classList.remove("disabled", "correct", "incorrect");
+  }
 }
 
-// Function to move to the next question
+// Function to show the next question
 function nextQuestion() {
-currentQuestion++;
-if (currentQuestion < totalQuestions) {
-enableAnswerButtons();
-showQuestion();
-document.getElementById("next-btn").classList.add("d-none");
-} else {
-endQuiz();
-}
-}
+  currentQuestion++;
 
-// Function to start the timer
-function startTimer() {
-timer = setInterval(updateTimer, 1000);
-}
-
-// Function to update the timer
-function updateTimer() {
-totalTime--;
-document.getElementById("timer").innerHTML = Time Remaining: ${totalTime}s;
-if (totalTime <= 0) {
-endQuiz();
-}
+  if (currentQuestion < totalQuestions) {
+    enableAnswerButtons();
+    showQuestion();
+    document.getElementById("next-btn").classList.add("d-none");
+  } else {
+    endQuiz();
+  }
 }
 
 // Function to end the quiz
 function endQuiz() {
-clearInterval(timer);
-document.getElementById("quiz-container").classList.add("d-none");
-document.getElementById("end-container").classList.remove("d-none");
-document.getElementById("score").innerHTML = You scored ${score} out of ${totalQuestions}!;
-document.getElementById("accuracy").innerHTML = Accuracy: ${(score / totalQuestions) * 100}%;
-document.getElementById("time-taken").innerHTML = Time taken: ${60 - totalTime}s;
+  clearInterval(timer);
+  document.getElementById("quiz-container").classList.add("d-none");
+  document.getElementById("end-container").classList.remove("d-none");
+
+  // Show score and feedback
+  const feedback = calculateFeedback();
+  document.getElementById("score").innerHTML = `${score} / ${totalQuestions}`;
+  document.getElementById("feedback").innerHTML = feedback;
+}
+
+// Function to calculate the feedback for the quiz taker
+function calculateFeedback() {
+  let feedback = "";
+
+  if (score === totalQuestions) {
+    feedback = "Great job! You got all the questions right!";
+  } else if (score > totalQuestions / 2) {
+    feedback = "Good job! You did well, but there is room for improvement.";
+  } else {
+    feedback = "Keep practicing. You'll get there!";
+  }
+
+  if (countdownPerQuestion) { // add time bonus if the countdown was per question
+    const timeBonus = calculateTimeBonus();
+    feedback += ` You finished with ${timeBonus} seconds left!`;
+  }
+
+  return feedback;
+}
+
+// Function to start the timer
+function startTimer() {
+  const timeDisplay = document.getElementById("time");
+
+  timer = setInterval(function() {
+    totalTime--;
+    timeDisplay.innerHTML = totalTime;
+    if (totalTime === 0) {
+      endQuiz();
+    }
+  }, 1000);
+}
+
+// Function to calculate the time bonus for finishing the quiz before the timer ends
+function calculateTimeBonus() {
+  const timeLeft = totalTime;
+  const timeBonus = timeLeft * 2; // 2 points per second
+  return timeBonus;
 }
 
 // Event Listeners
 document.getElementById("start-btn").addEventListener("click", startQuiz);
 document.getElementById("next-btn").addEventListener("click", nextQuestion);
-
-
-
 
 
 
