5ba9572da3155bb978e993343fad85c656410236
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-05-03 21:45:34 -0500

    Update helpScript.js

--- helpScript.js
+++ helpScript.js
@@ -1,8 +1,8 @@
-<!-- Initialze the Bootstrap navbar toggler -->
-
-
+<!-- Initialize the Bootstrap navbar toggler -->
+<script>
 $(document).ready(function(){
     $('.navbar-toggler').click(function(){
         $('body').toggleClass('open-menu');
     });
 });
+</script>
