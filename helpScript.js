51110e65b21235a7cfe3e0ce904b56f7454e9e75
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-05-05 04:18:13 -0500

    Update helpScript.js

--- helpScript.js
+++ helpScript.js
@@ -1,19 +1,14 @@
 
 
-
-
 fetch('navbar.html')
   .then(response => response.text())
   .then(data => {
     document.querySelector('#navbar').innerHTML = data;
+
+    $(document).ready(function() {
+      console.log($('.navbar-toggler')); // add this line
+      $('.navbar-toggler').click(function() {
+        $('#navbarNav').toggleClass('collapse');
+      });
+    });
   });
-
-
-
-
-$(document).ready(function(){
-  console.log($('.navbar-toggler')); // add this line
-  $('.navbar-toggler').click(function(){
-    $('#navbarNav').toggleClass('collapse');
-  });
-});
