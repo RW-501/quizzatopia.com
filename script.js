2517d358d0fdd53e33fcda1507aab7f8c3c03da6
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-05-06 06:21:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -152,8 +152,9 @@ var quiz_main_area = document.getElementById("quiz_main_area").innerHTML;
       countdown--;
 
       if (countdown < 0) {
+                clearInterval(countdownInterval);
+         console.log("Admm "+ quiz_main_area;
         document.getElementById("quiz_main_area").innerHTML = quiz_main_area;
-        clearInterval(countdownInterval);
         showQuestion(); // Call the showQuestion function after the ad ends
       }
     }, 1000);
