385c98aca71f7cf3264746c66684a145bee5758b
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-05-07 13:28:23 -0500

    Create helpScript2.js

---
+++ helpScript2.js
@@ -0,0 +1,18 @@
+fetch('/elements/navbar.html')
+  .then(response => response.text())
+  .then(data => {
+    document.querySelector('#navbar').innerHTML = data;
+
+    $(document).ready(function() {
+      console.log($('.navbar-toggler'));
+      $('.navbar-toggler').click(function() {
+        $('#navbarNav').toggleClass('collapse');
+      });
+    });
+  });
+
+fetch('/elements/footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.querySelector('#mainFooter').innerHTML = data;
+});
