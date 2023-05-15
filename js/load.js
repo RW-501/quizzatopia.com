934f4dfa24508d5c8ed265d69f9b27ad9d08cb51
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-05-15 14:35:24 -0500

    Update load.js

--- js/load.js
+++ js/load.js
@@ -104,3 +104,237 @@ function updatePointsAndRank() {
  //userInfo.pointsRewards = pointsRewards;
  // saveUserInfo(userInfo);
 }
+
+
+
+// Function to check if a badge has been earned
+function hasEarnedBadge(badgeId) {
+  const earnedBadges = JSON.parse(localStorage.getItem('earnedBadges')) || [];
+  return earnedBadges.includes(badgeId);
+}
+
+// Function to award a badge
+function awardBadge(badgeId) {
+  const earnedBadges = JSON.parse(localStorage.getItem('earnedBadges')) || [];
+  const maxQuantity = getBadgeMaxQuantity(badgeId);
+
+  if (!earnedBadges.includes(badgeId) && earnedBadges.length < maxQuantity) {
+    earnedBadges.push(badgeId);
+    localStorage.setItem('earnedBadges', JSON.stringify(earnedBadges));
+  }
+}
+
+// Helper function to get the maximum quantity of a badge
+function getBadgeMaxQuantity(badgeId) {
+  const badge = badges.find(badge => badge.id === badgeId);
+  return badge ? badge.maxQuantity || 1 : 1;
+}
+
+
+
+
+
+
+
+
+function awardQuizzesTakenBadges() {
+  const userInfo = getUserInfo();
+  const quizzesTaken = userInfo.quizzesTaken;
+
+  // Define the milestones and corresponding badge information
+const milestones = [
+  { count: 5, badge: { id: 2, name: "Quiz Enthusiast", description: "Awarded for taking 5 quizzes", quantity: 1, imageUrl: "/images/badges/badge-qcount-1.png", maxQuantity: 1 } },
+  { count: 10, badge: { id: 3, name: "Quiz Aficionado", description: "Awarded for taking 10 quizzes", quantity: 1, imageUrl: "/images/badges/badge-qcount-2.png", maxQuantity: 1 } },
+  { count: 20, badge: { id: 4, name: "Quiz Master", description: "Awarded for taking 20 quizzes", quantity: 1, imageUrl: "/images/badges/badge-qcount-3.png", maxQuantity: 1 } },
+  { count: 30, badge: { id: 5, name: "Quiz Guru", description: "Awarded for taking 30 quizzes", quantity: 1, imageUrl: "/images/badges/badge-qcount-4.png", maxQuantity: 1 } },
+  { count: 40, badge: { id: 6, name: "Quiz Legend", description: "Awarded for taking 40 quizzes", quantity: 1, imageUrl: "/images/badges/badge-qcount-5.png", maxQuantity: 1 } },
+  { count: 50, badge: { id: 7, name: "Quiz Champion", description: "Awarded for taking 50 quizzes", quantity: 1, imageUrl: "/images/badges/badge-qcount-6.png", maxQuantity: 1 } }
+];
+
+
+
+  // Check if the user has reached any of the milestones and hasn't already earned the corresponding badge
+  for (const milestone of milestones) {
+    if (quizzesTaken >= milestone.count && !hasEarnedBadge(milestone.badge.id)) {
+      awardBadge(milestone.badge);
+    }
+  }
+}
+
+const badges = [
+  { id: 1, name: "5- Day Badge", description: "Visiting 5 Consecutive Days", quantity: 1, maxQuantity: 10, imageUrl: "/images/badges/xxx-1.png", earnedDate: "" },
+  {
+    id: 8,
+    name: "Bronze Tier Badge",
+    description: "Earned by reaching the Bronze Tier (0-100 points).",
+    quantity: 1,
+    maxQuantity: 1,
+    imageUrl: "/images/badges/badge-qcount-8.png",
+    earnedDate: ""
+  },
+  {
+    id: 9,
+    name: "Silver Tier Badge",
+    description: "Earned by reaching the Silver Tier (101-250 points).",
+    quantity: 1,
+    maxQuantity: 1,
+    imageUrl: "/images/badges/badge-qcount-9.png",
+    earnedDate: ""
+  },
+  {
+    id: 10,
+    name: "Gold Tier Badge",
+    description: "Earned by reaching the Gold Tier (251-500 points).",
+    quantity: 1,
+    maxQuantity: 1,
+    imageUrl: "/images/badges/badge-qcount-10.png",
+    earnedDate: ""
+  },
+  {
+    id: 11,
+    name: "Platinum Tier Badge",
+    description: "Earned by reaching the Platinum Tier (501-1000 points).",
+    quantity: 1,
+    maxQuantity: 1,
+    imageUrl: "/images/badges/badge-qcount-11.png",
+    earnedDate: ""
+  },
+  {
+    id: 12,
+    name: "Diamond Tier Badge",
+    description: "Earned by reaching the Diamond Tier (1001+ points).",
+    quantity: 1,
+    maxQuantity: 1,
+    imageUrl: "/images/badges/badge-qcount-12.png",
+    earnedDate: ""
+  },
+  {
+    id: 13,
+    name: "Perfect Score",
+    description: "Scored 100% on Quiz",
+    quantity: 1,
+    maxQuantity: 100,
+    imageUrl: "/images/badges/xxx-1.png",
+    earnedDate: ""
+  }
+];
+
+
+
+
+
+
+function getUserPoints() {
+  const userInfo = getUserInfo();
+  return userInfo.points;
+}
+
+function checkTier() {
+  const userPoints = getUserPoints();
+  
+  if (userPoints >= 0 && userPoints <= 100) {
+    return badges.find(badge => badge.id === 8);
+  } else if (userPoints > 100 && userPoints <= 250) {
+    return badges.find(badge => badge.id === 9);
+  } else if (userPoints > 250 && userPoints <= 500) {
+    return badges.find(badge => badge.id === 10);
+  } else if (userPoints > 500 && userPoints <= 1000) {
+    return badges.find(badge => badge.id === 11);
+  } else if (userPoints > 1000) {
+    return badges.find(badge => badge.id === 12);
+  } else {
+    return null;
+  }
+}
+
+
+
+
+// Function to get the current date and time from API
+async function getCurrentDateTime() {
+  try {
+    const response = await fetch('https://worldtimeapi.org/api/ip');
+    const data = await response.json();
+    return data.datetime; // Assuming the API returns date and time in ISO 8601 format
+  } catch (error) {
+    console.error('Error fetching current date and time:', error);
+    return null;
+  }
+}
+
+// Function to check if the user has visited the page before
+function hasVisitedPage() {
+  const lastVisit = localStorage.getItem('lastVisit');
+  return !!lastVisit;
+}
+
+// Function to track consecutive days of visits
+function trackConsecutiveDays() {
+  const lastVisit = localStorage.getItem('lastVisit');
+  const currentDate = new Date();
+
+  if (lastVisit) {
+    const prevVisitDate = new Date(lastVisit);
+    const timeDiff = currentDate.getTime() - prevVisitDate.getTime();
+    const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+    if (daysDiff === 1) {
+      const consecutiveDays = parseInt(localStorage.getItem('consecutiveDays') || '0');
+      localStorage.setItem('consecutiveDays', consecutiveDays + 1);
+    } else if (daysDiff > 1) {
+      localStorage.removeItem('consecutiveDays');
+    }
+  }
+
+  localStorage.setItem('lastVisit', currentDate.toISOString());
+}
+
+
+
+
+
+
+
+// Function to reward points for each returning day up to 5 days
+function rewardPointsForReturningDays() {
+  const consecutiveDays = parseInt(localStorage.getItem('consecutiveDays') || '0');
+  const pointsPerDay = 10; // Points to be awarded per day
+
+  if (consecutiveDays < 5) {
+    const pointsEarned = consecutiveDays * pointsPerDay;
+   
+     updatePoints(pointsEarned);
+
+    // Update the user's points
+    // updatePoints(pointsEarned);
+    console.log(`Earned ${pointsEarned} points for returning ${consecutiveDays} day(s).`);
+  } else if (consecutiveDays === 5) {
+    const pointsEarned = consecutiveDays * pointsPerDay;
+    // Update the user's points
+    updatePoints(pointsEarned);
+    console.log(`Earned ${pointsEarned} points and the 5-Day badge.`);
+    awardBadge(1); // Assuming badge with ID 5 is the 5-Day badge
+  }
+}
+
+// Usage example
+async function checkUserVisit() {
+  const currentDateTime
+
+
+
+
+
+
+
+
+
+
+  
+  
+
+
+
+
+
+
