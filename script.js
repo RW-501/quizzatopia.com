46c29c72aeaef15b5eaddd7e10fc3a198cc47ade
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-05-16 11:07:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -525,7 +525,7 @@ displayEarnedPoints(earnedPoints);
 
    const newBadges = JSON.parse(localStorage.getItem('earnedBadges')) || [];
   
-let newNewBadge += newBadges;
+let newNewBadge = newBadges;
   // Check if any new badges were earned
   const newlyEarnedBadges = newNewBadge;
 
