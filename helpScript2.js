fa42cdfe8063bc0ccdb749635e8349c291ae21d8
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-05-16 13:24:37 -0500

    Update helpScript2.js

--- helpScript2.js
+++ helpScript2.js
@@ -1,23 +1,9 @@
-    <link rel="import" href="/js/load.js" >
 
 
 fetch('/elements/navbar2.html')
   .then(response => response.text())
   .then(data => {
     document.querySelector('#navbar').innerHTML = data;
-
-			    function displayUserInfoNav() {
-  const userInfo = getUserInfo();
-//  document.getElementById('userNameQ').innerHTML = userInfo.userName;
-//  document.getElementById('profile-name').innerHTML = userInfo.rank;
-//  document.getElementById('userPointsQ').innerHTML = userInfo.points;
-          
-  document.getElementById('profile-pic').src = userInfo.profilePic;
-
-  // Display additional user information as needed
-}
-		    
-		    displayUserInfoNav();
 
 	
 	
