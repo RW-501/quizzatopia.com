a79426c54736d8f1402d980fc43a3e872fa19f78
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-05-16 12:50:48 -0500

    Update helpScript.js

--- helpScript.js
+++ helpScript.js
@@ -16,3 +16,17 @@ fetch('./elements/footer.html')
   .then(data => {
     document.querySelector('#mainFooter').innerHTML = data;
 });
+
+
+		    function displayUserInfoNav() {
+  const userInfo = getUserInfo();
+//  document.getElementById('userNameQ').innerHTML = userInfo.userName;
+  document.getElementById('profile-name').innerHTML = userInfo.rank;
+//  document.getElementById('userPointsQ').innerHTML = userInfo.points;
+          
+  document.getElementById('profile-pic').src = userInfo.profilePic;
+
+  // Display additional user information as needed
+}
+		    
+		    displayUserInfoNav();
