d1dead6bea592b780e79362503a45f3332e55eb9
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-05-20 14:23:04 -0500

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -2,9 +2,15 @@
 
 // This file contains the authentication functionality for the login/signup pop-up
 
-// Firebase configuration
+// Firebase app configuration
 var firebaseConfig = {
-  // Your Firebase configuration details
+  apiKey: "AIzaSyC8PYJV5-E6hIYbElsgb5e7MOS0faCiLM4",
+  authDomain: "quizzatopia-bdfc9.firebaseapp.com",
+  projectId: "quizzatopia-bdfc9",
+  storageBucket: "quizzatopia-bdfc9.appspot.com",
+  messagingSenderId: "828105067102",
+  appId: "1:828105067102:web:76afb989ed7c03ebb542cf",
+  measurementId: "G-J3QK9V5480"
 };
 
 // Initialize Firebase
@@ -31,8 +37,32 @@ function handleLogin(event) {
   event.preventDefault();
   var email = document.getElementById('email').value;
   var password = document.getElementById('password').value;
+
   // Firebase authentication logic for email/password login
-  // Use Firebase Auth API to sign in the user with email and password
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Retrieve the user information
+      var user = userCredential.user;
+
+      // Get the user's display name and email
+      var displayName = user.displayName;
+      var email = user.email;
+
+      // Perform any additional actions or redirect the user
+
+      // Example: Show a success message and user info
+      alert(' email Login successful');
+      console.log('User display name:', displayName);
+      console.log('User email:', email);
+    })
+    .catch((error) => {
+      // Handle errors during login
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // Handle the error appropriately
+    });
 }
 
 // Function to handle signup form submission
@@ -47,12 +77,62 @@ function handleSignup(event) {
 
 // Function to handle Google sign-in
 function signInWithGoogle() {
-  // Firebase authentication logic for Google sign-in
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      // Retrieve the user information
+      var user = result.user;
+      
+      // Get the user's display name and email
+      var displayName = user.displayName;
+      var email = user.email;
+      
+      // Perform any additional actions or redirect the user
+      
+      // Example: Show a success message and user info
+      alert(' google Login successful');
+      console.log('User display name:', displayName);
+      console.log('User email:', email);
+    })
+    .catch((error) => {
+      // Handle errors during sign-in
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // Handle the error appropriately
+    });
 }
 
 // Function to handle Facebook sign-in
 function signInWithFacebook() {
-  // Firebase authentication logic for Facebook sign-in
+  var provider = new firebase.auth.FacebookAuthProvider();
+
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      // Retrieve the user information
+      var user = result.user;
+      
+      // Get the user's display name and email
+      var displayName = user.displayName;
+      var email = user.email;
+      
+      // Perform any additional actions or redirect the user
+      
+      // Example: Show a success message and user info
+      alert('facebook Login successful');
+      console.log('User display name:', displayName);
+      console.log('User email:', email);
+    })
+    .catch((error) => {
+      // Handle errors during sign-in
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // Handle the error appropriately
+    });
 }
 
 // Function to initialize the login/signup functionality
@@ -69,6 +149,7 @@ window.onload = initializeAuth;
 
 
 /*
+  <link rel="stylesheet" href="auth/style.css">
 
 <!-- HTML code to display the login/signup pop-up -->
 <div id="loginPopup" class="popup">
@@ -79,77 +160,17 @@ window.onload = initializeAuth;
 <script src="auth/auth.js"></script>
 
 
+    openPopup();
 
 
 
+<button id="openPopupButton">Open Login/Sign Up</button>
 
-
-
-// auth/auth.js
-
-// This file contains the authentication functionality for the login/signup pop-up
-
-// Firebase configuration
-var firebaseConfig = {
-  // Your Firebase configuration details
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Function to open the login/signup popup
-function openPopup() {
-  document.getElementById('loginPopup').style.display = 'block';
-}
-
-// Function to close the login/signup popup
-function closePopup() {
-  document.getElementById('loginPopup').style.display = 'none';
-}
-
-// Function to switch between login and signup tabs
-function switchTab(tabName) {
-  document.getElementById('loginTab').style.display = tabName === 'login' ? 'block' : 'none';
-  document.getElementById('signupTab').style.display = tabName === 'signup' ? 'block' : 'none';
-}
-
-// Function to handle login form submission
-function handleLogin(event) {
-  event.preventDefault();
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
-  // Firebase authentication logic for email/password login
-  // Use Firebase Auth API to sign in the user with email and password
-}
-
-// Function to handle signup form submission
-function handleSignup(event) {
-  event.preventDefault();
-  var username = document.getElementById('username').value;
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
-  // Firebase authentication logic for email/password signup
-  // Use Firebase Auth API to create a new user with email and password
-}
-
-// Function to handle Google sign-in
-function signInWithGoogle() {
-  // Firebase authentication logic for Google sign-in
-}
-
-// Function to handle Facebook sign-in
-function signInWithFacebook() {
-  // Firebase authentication logic for Facebook sign-in
-}
-
-// Function to initialize the login/signup functionality
-function initializeAuth() {
-  document.getElementById('loginForm').addEventListener('submit', handleLogin);
-  document.getElementById('signupForm').addEventListener('submit', handleSignup);
-}
-
-// Call the initializeAuth function when the window finishes loading
-// window.onload = initializeAuth;
+<script>
+  document.getElementById('openPopupButton').addEventListener('click', function() {
+    openPopup();
+  });
+</script>
 
 
 */
