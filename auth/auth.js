f1edd8db00f5215d26d8e0eb1f82a454f69f3fd3
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-05-21 04:05:46 -0500

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -2,7 +2,16 @@
 // Function to open the login/signup popup
 
 
-
+const firebaseConfig = {
+  // Your Firebase configuration
+  apiKey: "AIzaSyC8PYJV5-E6hIYbElsgb5e7MOS0faCiLM4",
+  authDomain: "quizzatopia-bdfc9.firebaseapp.com",
+  projectId: "quizzatopia-bdfc9",
+  storageBucket: "quizzatopia-bdfc9.appspot.com",
+  messagingSenderId: "828105067102",
+  appId: "1:828105067102:web:76afb989ed7c03ebb542cf",
+  measurementId: "G-J3QK9V5480"
+};
 
 
 // Function to handle Google sign-in
