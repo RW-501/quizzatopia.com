cd15cbc0c4afa2e03ff4d00f6eaaf110911fce30
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-05-20 20:33:53 -0500

    Update app.js

--- auth/app.js
+++ auth/app.js
@@ -1,7 +1,7 @@
 // app.js
 
-import { initializeApp } from 'auth/app.js';
-import 'auth/auth.js';
+import { initializeApp } from '/auth/app.js';
+import '/auth/auth.js';
 
 const firebaseConfig = {
   // Your Firebase configuration
