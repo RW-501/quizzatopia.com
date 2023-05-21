7d45ba26c2cec10b7fde5f9d1e8661b42f6be525
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-05-20 20:12:49 -0500

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -32,37 +32,6 @@ export function signInWithGoogle() {
     });
 }
 
-// Function to handle Facebook sign-in
-export function signInWithFacebook() {
-  const provider = new firebase.auth.FacebookAuthProvider();
-
-  return firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then((result) => {
-      // Retrieve the user information
-      const user = result.user;
-
-      // Get the user's display name and email
-      const displayName = user.displayName;
-      const email = user.email;
-
-      // Perform any additional actions or redirect the user
-
-      // Example: Show a success message and user info
-      alert('Facebook Login successful');
-      console.log('User display name:', displayName);
-      console.log('User email:', email);
-    })
-    .catch((error) => {
-      // Handle errors during sign-in
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // Handle the error appropriately
-    });
-}
-
-// ...
 
 
 // Function to handle Facebook sign-in
