14fb35b8d46633fcf971c77d6169e54a96ed7f91
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-05-23 05:56:20 -0500

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -184,21 +184,7 @@ function saveUserInfoToFirestore(userInfo) {
 }
 
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC8PYJV5-E6hIYbElsgb5e7MOS0faCiLM4",
-  authDomain: "quizzatopia-bdfc9.firebaseapp.com",
-  projectId: "quizzatopia-bdfc9",
-  storageBucket: "quizzatopia-bdfc9.appspot.com",
-  messagingSenderId: "828105067102",
-  appId: "1:828105067102:web:76afb989ed7c03ebb542cf",
-  measurementId: "G-J3QK9V5480"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
-// Get a reference to the Firestore database
-const firestore = firebase.firestore();
 
   // Access the necessary functions
   const auth = firebase.auth();
