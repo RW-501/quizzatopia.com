897b8c47d9434f9eb558a8f0d681efa14ce1246f
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-05-23 12:03:43 -0500

    Update helpScript2.js

--- helpScript2.js
+++ helpScript2.js
@@ -11,7 +11,27 @@ fetch('/elements/navbar2.html')
  console.log('navUserInfo.profilePic 1 ', navUserInfo.profilePic);
   document.getElementById('profile-pic').src = navUserInfo.profilePic;
    
+// Check if user is logged in
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is logged in
+    document.getElementById('navLoggedin').innerHTML = "<button onclick=\"logOutFunction()\">Log Out</button>";
+  } else {
+    // User is not logged in
+    document.getElementById('navLoggedin').innerHTML = "<button onclick=\"openPopup()\">Log In</button>";
+  }
+});
 
+// Log out function
+function logOutFunction() {
+  firebase.auth().signOut().then(function() {
+    // Log out successful
+    console.log("User logged out.");
+  }).catch(function(error) {
+    // An error occurred
+    console.log("Error logging out:", error);
+  });
+}
 
 	
     $(document).ready(function() {
