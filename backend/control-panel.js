9f483bc55571c069f321ec732d56f95839a99d99
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-06-30 22:57:19 -0500

    Update control-panel.js

--- backend/control-panel.js
+++ backend/control-panel.js
@@ -1,9 +1,10 @@
+/*
 // Get the current URL path
 var path = window.location.pathname;
 // Find the corresponding navigation link and add 'active' class
 $('.nav-link[href="' + path + '"]').parent().addClass('active');
 
-
+*/
 
 function getBadWords() {
   const db = firebase.firestore();
