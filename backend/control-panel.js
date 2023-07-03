1d2b0a48b4f969548d056c4b46968473974aaa11
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-07-02 19:13:00 -0500

    Update control-panel.js

--- backend/control-panel.js
+++ backend/control-panel.js
@@ -133,13 +133,13 @@ function renderUserStatsChart() {
 // Function to fetch user data and populate the user table
 function populateUserTable() {
   // Reference to the 'users' collection in Firebase Firestore
-  const users = firebase.firestore().collection('users');
+  const usersRef = firebase.firestore().collection('users');
 
   // Reference to the 'userTable' tbody element
   const userTableBody = document.querySelector('#userTable tbody');
 
   // Fetch user data from Firestore
-  users.get()
+  usersRef.get()
     .then((querySnapshot) => {
       // Clear existing table rows
       userTableBody.innerHTML = '';
@@ -165,10 +165,25 @@ function populateUserTable() {
         const emailCell = document.createElement('td');
         emailCell.textContent = userEmail;
 
-        // Append cells to the new row
+        // Create buttons for deactivating and removing users
+        const deactivateButton = document.createElement('button');
+        deactivateButton.textContent = 'Deactivate';
+        deactivateButton.addEventListener('click', () => {
+          deactivateUser(doc.id); // Call the deactivateUser function with the document ID
+        });
+
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Remove';
+        removeButton.addEventListener('click', () => {
+          removeUser(doc.id); // Call the removeUser function with the document ID
+        });
+
+        // Append cells and buttons to the new row
         newRow.appendChild(idCell);
         newRow.appendChild(nameCell);
         newRow.appendChild(emailCell);
+        newRow.appendChild(deactivateButton);
+        newRow.appendChild(removeButton);
 
         // Append the new row to the table body
         userTableBody.appendChild(newRow);
@@ -180,6 +195,51 @@ function populateUserTable() {
     });
 }
 
+function deactivateUser(userId) {
+  // Reference to the 'users' collection in Firebase Firestore
+  const usersRef = firebase.firestore().collection('users');
+
+  // Update the user document with the provided userId
+  usersRef.doc(userId).update({
+    isActive: false // Set the 'isActive' field to false
+  })
+    .then(() => {
+      console.log('User deactivated successfully');
+    })
+    .catch((error) => {
+      console.error('Error deactivating user:', error);
+    });
+}
+
+function removeUser(userId) {
+  // Reference to the 'users' collection in Firebase Firestore
+  const usersRef = firebase.firestore().collection('users');
+
+  // Remove the user document with the provided userId
+  usersRef.doc(userId).delete()
+    .then(() => {
+      console.log('User removed successfully');
+    })
+    .catch((error) => {
+      console.error('Error removing user:', error);
+    });
+}
+
+function getTotalUsers() {
+  // Reference to the 'users' collection in Firebase Firestore
+  const usersRef = firebase.firestore().collection('users');
+
+  // Fetch all user documents and count the number of documents
+  usersRef.get()
+    .then((querySnapshot) => {
+      const totalUsers = querySnapshot.size;
+      console.log('Total users:', totalUsers);
+    })
+    .catch((error) => {
+      console.error('Error fetching user data:', error);
+    });
+}
+
 
 window.addEventListener("load", function() {
 // Call the getBadWords function
@@ -187,7 +247,7 @@ getBadWords();
 	// Call the functions to populate the components when the page loads or as needed
 renderUserStatsChart();
 populateUserTable();
-
+getTotalUsers();
 
 });
 
