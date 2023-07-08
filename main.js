5e69ec20bf85f1993db1b1bb88dcfa0e88905b17
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-07-08 17:52:26 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1620,7 +1620,7 @@ function logOutFunction() {
 // 
 
 function logVisitorInformation() {
-  const visitorIp = "172.223.110.58";// getIPAddress();
+  let visitorIp =  getIPAddress();
   const currentTimestamp = new Date();
   const referralPage = document.referrer;
   const userAgentString = navigator.userAgent;
