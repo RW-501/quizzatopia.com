c6e0b67c8e96b408b4924a3487d58b67c4ab84be
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-07-22 23:26:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -409,23 +409,6 @@ document.addEventListener('DOMContentLoaded', function() {
   fetchAndInsertContent();
 });
 
-// Function to view the user dashboard or show the login popup
-function viewDashboard() {
-  if (loggedIn === true) {
-    window.location.href = '/user/'; // Redirect to the user dashboard
-  } else {
-      console.log("test viewDashboard:");
-
-    // Show the login popup or switch to the login tab in the popup
-   //  openPopup();
-   // switchTab('login');
-
-      // Show the login popup
-     			slideIn("loginPopupBody");
-openPopup();switchTab('login');
-	  
-  }
-}
 
 
 
