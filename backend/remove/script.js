4ecb9a20f849f3b9c088e878426e052030dfd2d8
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-08-01 22:51:07 -0500

    Create script.js

---
+++ backend/remove/script.js
@@ -0,0 +1,30 @@
+
+// Function to get users without "firebaseId"
+async function getUsersWithoutFirebaseId() {
+  const querySnapshot = await db.collection('users').where('firebaseId', '==', '').get();
+  const users = [];
+
+  querySnapshot.forEach((doc) => {
+    users.push({
+      id: doc.id,
+      data: doc.data(),
+    });
+  });
+
+  return users;
+}
+
+// Function to remove a user by ID
+async function removeUser(userId) {
+  try {
+    await db.collection('users').doc(userId).delete();
+    console.log('User removed successfully.');
+  } catch (error) {
+    console.error('Error removing user:', error);
+  }
+}
+
+module.exports = {
+  getUsersWithoutFirebaseId,
+  removeUser,
+};
