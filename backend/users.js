de6fbaf658d641c170a4202aead954b5b61b25db
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-08-21 16:06:25 -0500

    Update users.js

--- backend/users.js
+++ backend/users.js
@@ -1,17 +1,36 @@
 // Function to fetch user data and populate the user table
-function populateUserTable(sortField) {
+function populateUserTable(sortField, sortOrder) {
   const usersRef = firebase.firestore().collection('users');
   const userTableBody = document.querySelector('#userTable tbody');
+  const userCountElement = document.querySelector('#userCount');
 
-usersRef.orderBy(sortField,  'desc').limit(10).get()
+  // Define the date format function
+  function formatDate(timestamp) {
+    const now = new Date();
+    const date = timestamp.toDate();
+    const diffInMinutes = Math.floor((now - date) / (1000 * 60));
+    
+    if (diffInMinutes < 60) {
+      return `${diffInMinutes} min ago`;
+    } else if (diffInMinutes < 1440) {
+      const diffInHours = Math.floor(diffInMinutes / 60);
+      return `${diffInHours} hour${diffInHours > 1 ? 's' : ''} ago`;
+    } else {
+      const diffInDays = Math.floor(diffInMinutes / 1440);
+      return `${diffInDays} day${diffInDays > 1 ? 's' : ''} ago`;
+    }
+  }
+
+  usersRef.orderBy(sortField, sortOrder).limit(10).get()
     .then((querySnapshot) => {
       userTableBody.innerHTML = '';
+      let userCount = 0;
 
       querySnapshot.forEach((doc) => {
         const user = doc.data();
-        const userJoinedDate = doc.get('userJoinedDate');
-        const userName = user.userName;
-        const userEmail = user.userEmail;
+        const userJoinedDate = formatDate(doc.get('userJoinedDate'));
+        const userName = user.userName || 'Anonymous';
+        const userEmail = user.userEmail || 'Anonymous';
         const userQuizzesTaken = user.userQuizzesTaken || 0;
         const userPoints = user.userPoints || 0;
 
@@ -46,7 +65,11 @@ usersRef.orderBy(sortField,  'desc').limit(10).get()
         newRow.appendChild(actionsCell);
 
         userTableBody.appendChild(newRow);
+
+        userCount++;
       });
+
+      userCountElement.textContent = `Total Users: ${userCount}`;
     })
     .catch((error) => {
       console.error('Error fetching user data:', error);
@@ -69,11 +92,28 @@ function createActionButton(label, clickHandler) {
   return button;
 }
 
-function sortTable(sortField) {
-  populateUserTable(sortField);
+// Function to toggle sorting direction
+function toggleSortDirection() {
+  const sortOrder = sortOrder === 'asc' ? 'desc' : 'asc';
+  return sortOrder;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  // Populate the table with default sorting by userName
-  populateUserTable('userName');
+// Event listener for sorting buttons
+const sortButtons = document.querySelectorAll('.sort-button');
+sortButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const sortField = button.dataset.sortField;
+    sortOrder = toggleSortDirection();
+    populateUserTable(sortField, sortOrder);
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Populate the table with default sorting by userName and ascending order
+  populateUserTable('userName', 'asc');
+
+  // Update the table every 5 minutes
+  setInterval(() => {
+    populateUserTable('userName', sortOrder);
+  }, 5 * 60 * 1000); // 5 minutes in milliseconds
 });
