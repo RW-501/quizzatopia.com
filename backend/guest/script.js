fa7eafb81ee9fbc03f4779318623f7951e3f298a
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-08-21 15:24:45 -0500

    Update script.js

--- backend/guest/script.js
+++ backend/guest/script.js
@@ -3,7 +3,7 @@ function populateGuestLogTable(sortField) {
   const guestLogRef = firebase.firestore().collection('guestLog');
   const guestLogTableBody = document.querySelector('#guestLogTable tbody');
 
-guestLogRef.orderBy(sortField,  'desc').limit(20).get()
+  guestLogRef.orderBy(sortField, 'desc').limit(20).get()
     .then((querySnapshot) => {
       guestLogTableBody.innerHTML = '';
 
@@ -19,6 +19,11 @@ guestLogRef.orderBy(sortField,  'desc').limit(20).get()
         const referralPage = guest.referralPage;
         const userVisitCount = guest.userVisitCount || 0;
 
+        // Calculate the time elapsed since the last visit
+        const currentTime = new Date();
+        const lastVisitDateTime = new Date(guest.lastVisitTime.toDate());
+        const timeElapsed = calculateTimeElapsed(currentTime, lastVisitDateTime);
+
         const newRow = document.createElement('tr');
         newRow.classList.add('guest-row');
 
@@ -29,6 +34,10 @@ guestLogRef.orderBy(sortField,  'desc').limit(20).get()
         const firstVisitTimeCell = createTableCell(firstVisitTime);
         const lastVisitPageCell = createTableCell(lastVisitPage);
         const lastVisitTimeCell = createTableCell(lastVisitTime);
+
+        // Create a cell for time elapsed since last visit
+        const timeElapsedCell = createTableCell(timeElapsed);
+
         const referralPageCell = createTableCell(referralPage);
         const userVisitCountCell = createTableCell(userVisitCount);
 
@@ -39,6 +48,7 @@ guestLogRef.orderBy(sortField,  'desc').limit(20).get()
         newRow.appendChild(firstVisitTimeCell);
         newRow.appendChild(lastVisitPageCell);
         newRow.appendChild(lastVisitTimeCell);
+        newRow.appendChild(timeElapsedCell); // Add the time elapsed cell
         newRow.appendChild(referralPageCell);
         newRow.appendChild(userVisitCountCell);
 
@@ -55,6 +65,23 @@ function createTableCell(content) {
   const cell = document.createElement('td');
   cell.textContent = content;
   return cell;
+}
+
+// Helper function to calculate time elapsed between two dates
+function calculateTimeElapsed(currentTime, lastVisitTime) {
+  const timeDifference = currentTime - lastVisitTime;
+  const seconds = Math.floor((timeDifference / 1000) % 60);
+  const minutes = Math.floor((timeDifference / 1000 / 60) % 60);
+  const hours = Math.floor((timeDifference / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+  let timeElapsed = '';
+  if (days > 0) timeElapsed += `${days}d `;
+  if (hours > 0) timeElapsed += `${hours}h `;
+  if (minutes > 0) timeElapsed += `${minutes}m `;
+  if (seconds > 0) timeElapsed += `${seconds}s`;
+
+  return timeElapsed;
 }
 
 function sortTable(sortField) {
