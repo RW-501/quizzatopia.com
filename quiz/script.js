72658a2b6e27178bfd553c7f256a1c55c777e533
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-08-21 23:41:06 -0500

    Update script.js

--- quiz/script.js
+++ quiz/script.js
@@ -1600,13 +1600,6 @@ window.addEventListener('beforeunload', function(event) {
 });
 var userExitNotificationBool = false;
 
-window.addEventListener('beforeunload', function (event) {
-  // Show exit notification or confirmation message here
-  if (quizStarted === true) {
-    event.returnValue = "Are you sure you want to leave? Your progress in the quiz will be lost.";
-  }
-});
-
 
 
 
