ee7806bd2159328951f9265f7a6e167f1e5f5f2c
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-08-26 18:08:03 -0500

    Update main.js

--- backend/weeklyUserChallenges/main.js
+++ backend/weeklyUserChallenges/main.js
@@ -33,9 +33,59 @@ firestore.collection("weeklyUserChallenges")
     messageDiv.innerHTML = "Error adding challenge data.";
   });
 
+
         // Clear the form fields
         document.getElementById("user-id").value = "";
         document.getElementById("week-start-date").value = "";
         document.getElementById("challenges").value = "";
     });
 });
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("challenge-form");
+    const messageDiv = document.getElementById("message");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const userId = document.getElementById("user-id").value;
+        const weekStartDate = document.getElementById("week-start-date").value;
+        const challengesStr = document.getElementById("challenges").value;
+        const challengeIds = challengesStr.split(",").map(id => id.trim());
+
+        // Parse JSON data from the <textarea>
+        const challengeJson = document.getElementById("challenge-json").value;
+        let challengeData;
+
+        try {
+            challengeData = JSON.parse(challengeJson);
+        } catch (error) {
+            console.error("Error parsing JSON: ", error);
+            messageDiv.innerHTML = "Error parsing challenge data. Make sure it's valid JSON.";
+            return;
+        }
+
+        // Construct the document data
+        const userChallenges = {
+            userID: userId,
+            weekStartDate: weekStartDate,
+            challenges: challengeData // Use the parsed JSON data
+        };
+
+        // Add or update the document in Firebase Firestore
+        // Replace the following with your Firebase Firestore logic
+        // ...
+
+        // Clear the form fields and textarea
+        document.getElementById("user-id").value = "";
+        document.getElementById("week-start-date").value = "";
+        document.getElementById("challenges").value = "";
+        document.getElementById("challenge-json").value = "";
+    });
+});
+
