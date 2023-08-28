5f7302331c25b230185e788b74e4cc3074a55216
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-08-28 16:03:49 -0500

    Update lateLoad.js

--- elements/lateLoad.js
+++ elements/lateLoad.js
@@ -1531,7 +1531,6 @@ window.addEventListener("load", () => {
 		    const userInfo = getUserInfo();
 
     const firebaseId = userInfo.firebaseId;
-    const ipAddress = ipAddress; // Implement this function to get the user's IP address
     const currentUrl = window.location.href;
 
     logUserMovement(ipAddress, firebaseId, currentUrl);
